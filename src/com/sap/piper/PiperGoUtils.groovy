c561891a13bae9827d95efad5c3827effa41e6e0
Srinikitha Kondreddy <srinikitha.kondreddy@sap.com>
2021-03-17 13:05:25 +0100

    Use my own piper go bin for testing.

--- src/com/sap/piper/PiperGoUtils.groovy
+++ src/com/sap/piper/PiperGoUtils.groovy
@@ -31,12 +31,12 @@ class PiperGoUtils implements Serializable {
             ]) {
                 def piperTar = 'piper-go.tar.gz'
                 def piperTmp = 'piper-tmp'
-                steps.sh "wget --output-document ${piperTar} https://github.com/\${REPOSITORY_UNDER_TEST}/archive/\$LIBRARY_VERSION_UNDER_TEST.tar.gz"
-                steps.sh "PIPER_TMP=${piperTmp}; rm -rf \${PIPER_TMP} && mkdir -p \${PIPER_TMP} && tar --strip-components=1 -C \${PIPER_TMP} -xf ${piperTar}"
-                steps.dir(piperTmp) {
-                    steps.sh "CGO_ENABLED=0 go build -tags release -ldflags \"-X github.com/SAP/jenkins-library/cmd.GitCommit=${steps.env.LIBRARY_VERSION_UNDER_TEST}\" -o ../piper . && chmod +x ../piper && chown 1000:999 ../piper"
-                }
-                steps.sh "rm -rf ${piperTar} ${piperTmp}"
+                steps.sh "curl -L https://github.com/srinikitha09/jenkins-library/releases/download/v999/piper"
+               // steps.sh "PIPER_TMP=${piperTmp}; rm -rf \${PIPER_TMP} && mkdir -p \${PIPER_TMP} && tar --strip-components=1 -C \${PIPER_TMP} -xf ${piperTar}"
+               // steps.dir(piperTmp) {
+               //     steps.sh "CGO_ENABLED=0 go build -tags release -ldflags \"-X github.com/srinikitha09/jenkins-library/cmd.GitCommit=${steps.env.LIBRARY_VERSION_UNDER_TEST}\" -o ../piper . && chmod +x ../piper && chown 1000:999 ../piper"
+               // }
+               // steps.sh "rm -rf ${piperTar} ${piperTmp}"
             }
         } else {
             def libraries = getLibrariesInfo()
@@ -48,7 +48,7 @@ class PiperGoUtils implements Serializable {
             }
 
             def fallbackUrl = 'https://github.com/SAP/jenkins-library/releases/latest/download/piper_master'
-            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/SAP/jenkins-library/releases/download/${version}/piper"
+            def piperBinUrl = "https://github.com/srinikitha09/jenkins-library/releases/download/v999/piper"
 
             boolean downloaded = downloadGoBinary(piperBinUrl)
             if (!downloaded) {
